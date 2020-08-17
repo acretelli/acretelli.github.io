9b2be5d2cf184b90ceb75fe180a69776d822b91c
Anna Fernandes <anna.cbf@gmail.com>
2020-08-17 15:30:04 -0300

    Deletei site antigo e criei versão react em manutenção

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Anna Fernandes | Portfólio</h2>
+        <p>
+          Site em manutenção
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/annacbfernandes/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Likedin
+        </a>
+        <a
+          className="App-link"
+          href="https://www.behance.net/annacbfernandes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Behance
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
